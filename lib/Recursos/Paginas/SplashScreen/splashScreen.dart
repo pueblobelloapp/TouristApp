a65b59ed4b52b1063d62b8c7944c2948813c3354
JoseAlvrez <109702179+JoseAlvrez@users.noreply.github.com>
2023-06-11 22:56:35 -0500

    Estructuración del código de la vista splashScreen respecto al mockup

--- lib/Recursos/Paginas/SplashScreen/splashScreen.dart
+++ lib/Recursos/Paginas/SplashScreen/splashScreen.dart
@@ -1,6 +1,9 @@
+// ignore_for_file: unnecessary_null_comparison
+
 import 'dart:async';
 
 import 'package:app_turismo_usuario/Recursos/Paginas/My_location/my_location.dart';
+import 'package:app_turismo_usuario/Recursos/theme/app_theme.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
@@ -31,7 +34,7 @@ class _SplashScreenState extends State<SplashScreen>
       })
       ..addStatusListener((status) {
         if (status == AnimationStatus.completed) {
-          Future.delayed(const Duration(seconds: 4), () {
+          Future.delayed(const Duration(seconds: 2), () {
             _onShowLogin();
           });
         }
@@ -57,61 +60,27 @@ class _SplashScreenState extends State<SplashScreen>
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        height: double.infinity,
-        width: double.infinity,
-        decoration: const BoxDecoration(
-            gradient: LinearGradient(colors: [
-          Color.fromRGBO(125, 164, 83, 9.0),
-          Color.fromRGBO(242, 238, 237, 0.0)
-        ], begin: Alignment.topCenter, end: Alignment.bottomCenter)),
-        child: Center(
+        backgroundColor: AppBasicColors.rgb,
+        body: Center(
           child: Column(
-            children: [
-              Center(
-                  child: Column(
-                children: <Widget>[
-                  const Padding(padding: EdgeInsets.symmetric(vertical: 110)),
-                  Transform.scale(
-                    scale: _animation == null ? 1.0 : _animation.value,
-                    child: const Text(
-                      'Turismo App',
-                      style:
-                          TextStyle(fontSize: 40, fontWeight: FontWeight.bold),
-                    ),
-                  ),
-                  const SizedBox(
-                    height: 10,
-                  ),
-                  AnimatedContainer(
-                    duration: const Duration(seconds: 4),
-                    curve: Curves.easeInOut,
-                    margin: const EdgeInsets.all(00.0),
-                    width: _animation == null
-                        ? MediaQuery.of(context).size.width * 0.85
-                        : _animation.value *
-                            MediaQuery.of(context).size.width *
-                            0.85,
-                    height: _animation == null
-                        ? MediaQuery.of(context).size.width * 0.85
-                        : _animation.value *
-                            MediaQuery.of(context).size.width *
-                            0.85,
-                    decoration: const BoxDecoration(
-                        //color: Colors.amber,
-                        image: DecorationImage(
-                            image: AssetImage(
-                              'Assets/Img/Logo.png',
-                            ),
-                            fit: BoxFit.contain)),
-                  ),
-                  const SizedBox(height: 40),
-                ],
-              )),
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Transform.scale(
+                scale: _animation == null ? 0.3 : _animation.value,
+                child: const Text(
+                  'IKU',
+                  style: TextStyle(fontSize: 60, fontWeight: FontWeight.bold),
+                ),
+              ),
+              Transform.scale(
+                scale: _animation == null ? 1.0 : _animation.value,
+                child: const Text('Pueblo Bello',
+                    style: TextStyle(
+                      fontSize: 20, //fontWeight:FontWeight.bold
+                    )),
+              )
             ],
           ),
-        ),
-      ),
-    );
+        ));
   }
 }
